05383a82c596c466cc95aae1af59bbcdbdec3f1a
rileyellwanger <54553967+rileyellwanger@users.noreply.github.com>
2019-08-29 17:14:47 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Riley Ellwanger and Andrew Kwarcinski
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Riley Ellwanger and Andrew Kwarcinski\n");
+
+  return 0;
+}
